2920753aa7c9d65dd22d878ea263fdeb66b2aed2
starry1441 <3363654551@qq.com>
2021-03-27 15:25:30 +0800

    3——27

---
+++ leecode/src/work3_27/Work2.java
@@ -0,0 +1,56 @@
+package work3_27;
+
+import work3_7.TreeNode;
+
+import java.util.*;
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: starry
+ * Date: 2021 -03 -27
+ * Time: 15:21
+ */
+public class Work2 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if(root == null) return new ArrayList();
+        Stack<TreeNode> a = new Stack<>();
+        List<List<Integer>> out = new ArrayList<>();
+        a.push(root);
+        Stack<TreeNode> b = new Stack<>();
+        int step = 0;
+        while(!a.empty() || !b.empty()) {
+            List<Integer> in = new ArrayList<>();
+            if(step%2 == 0) {
+                while(!a.empty()) {
+                    TreeNode cur = a.pop();
+                    in.add(cur.val);
+                    if(cur.left != null) {
+                        b.push(cur.left);
+                    }
+                    if(cur.right != null) {
+                        b.push(cur.right);
+                    }
+                }
+            }
+            if(step%2 == 1) {
+                while(!b.empty()) {
+                    TreeNode cur = b.pop();
+                    in.add(cur.val);
+                    if(cur.right != null) {
+                        a.push(cur.right);
+                    }
+                    if(cur.left != null) {
+                        a.push(cur.left);
+                    }
+                }
+            }
+            step++;
+            out.add(in);
+        }
+        return out;
+    }
+
+}
