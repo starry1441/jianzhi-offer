60f98e892d0277271cc46e3a75def4ec0d53bf41
starry1441 <3363654551@qq.com>
2021-03-28 21:00:35 +0800

    3_28

---
+++ leecode/src/work3_28/Work1.java
@@ -0,0 +1,33 @@
+package work3_28;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: starry
+ * Date: 2021 -03 -28
+ * Time: 20:59
+ */
+public class Work1 {
+
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                map.put(nums[i],map.get(nums[i])+1);
+            }else {
+                map.put(nums[i],1);
+            }
+        }
+        int max = 0;
+        for(Integer key : map.keySet()) {
+            if(map.get(key) > nums.length/2) {
+                return key;
+            }
+        }
+        return -1;
+    }
+
+}
