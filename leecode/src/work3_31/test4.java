5f62722abd406e097be279a19888f39bb227b770
starry1441 <3363654551@qq.com>
2021-04-01 10:39:22 +0800

    4_1

---
+++ leecode/src/work3_31/test4.java
@@ -0,0 +1,16 @@
+package work3_31;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: starry
+ * Date: 2021 -04 -01
+ * Time: 10:38
+ */
+public class test4 {
+
+    public static void main(String[] args) {
+        System.out.println("sad");
+    }
+
+}
